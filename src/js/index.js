b372b0f936f03b8d4cb85fa9862c2f67f195a3f5
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-21 13:45:26 -0400

    added code to to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,21 @@
 "use strict";
 
-const accordion = document.querySelectorAll("#accordion");
-const panel = document.querySelectorAll("#panel");
-console.log(accordion);
-console.log(panel);
+const accordion = document.querySelectorAll(".accordion");
+const panelArrow = document.querySelectorAll(".panel-arrow");
 
-accordion.forEach((accordion) => {
-    accordion.addEventListener("click", () => {
-        accordion.classList.toggle("active");
+accordion.forEach((accord) => {
+    accord.addEventListener("click", () => {
+        const accordionBody = accord.nextElementSibling;
+        accord.classList.toggle("active");
+        accord.classList.contains("active")
+            ? (accordionBody.style.display = "block")
+            : (accordionBody.style.display = "none");
+
+        panelArrow.forEach((arrow) => {
+            const accArrow = arrow.parentElement;
+            accArrow.classList.contains("active")
+                ? arrow.classList.add("active")
+                : arrow.classList.remove("active");
+        });
     });
 });
